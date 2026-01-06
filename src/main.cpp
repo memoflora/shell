47fd6dde6df934602e7b246e4e0509dd68d3ea38
memoflora <fathmubina09@gmail.com>
2026-01-07 00:00:04 +0700

    Implement a REPL

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    while (true) {
+        std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";
+        std::string input;
+        std::getline(std::cin, input);
+        std::cout << input << ": command not found\n";
+    }
 }
